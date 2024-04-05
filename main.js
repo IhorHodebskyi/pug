dc528ba0a0a243e294b2550824156478317193b3
ihor <i0962112822@gmail.com>
2024-04-05 20:59:59 +0300

    add

--- main.js
+++ main.js
@@ -2,20 +2,46 @@ const fs = require("fs");
 const pug = require("pug");
 const age = require("./age");
 
-const compileFunc = pug.compileFile("layout.pug");
+// const compileFunc = pug.compileFile("layout.pug");
+
+let users = [];
 
 fs.readdirSync("users").forEach((fileName) => {
+  const id = fileName.replace(".json", "");
   const userDta = JSON.parse(fs.readFileSync("users/" + fileName, "utf-8"));
-
-  const data = {
-    ...{ age },
-    ...userDta,
-  };
-  console.log(data);
-  const html = compileFunc(data);
-
-  fs.writeFileSync("out/" + fileName.replace(".json", ".html"), html);
+  users.push(Object.assign({ id }, userDta));
 });
 
-// const html = pug.renderFile("layout.pug");
-// fs.writeFileSync("index.html", html);
+function buildUserList() {
+  const data = {
+    base: {
+      title: "Users",
+      pageClass: "list,",
+    },
+    users,
+  };
+  const html = pug.renderFile("layout/index.pug", data);
+  fs.writeFileSync("out/index.html", html);
+}
+
+function buildUsers() {
+  let compileFunc = pug.compileFile("layout/user.pug");
+
+  users.forEach((userData) => {
+    let data = {
+      base: {
+        title: userData.name,
+        pageClass: "user",
+      },
+      calcAge: age,
+      user: userData,
+    };
+
+    let html = compileFunc(data);
+    fs.writeFileSync("out/" + userData.id + ".html", html);
+  });
+}
+
+buildUserList();
+
+buildUsers();
